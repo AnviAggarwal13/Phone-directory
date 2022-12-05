38f66faec4a22325077088d8006703968cc1bff6
AnviAggarwal13 <anviaggarwal13@gmail.com>
2022-12-06 02:12:38 +0530

    Created skeleton of home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
-
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+import React, { Component } from "react";
+class App extends Component {
+  render() {
+    return (
+      <>
+        {/* <div className="header">
+          Phone Directory
+        </div>
+        <button>Add</button>
+        <div>
+          <span>Name</span><br />
+          <span>Phone</span>
+        </div> */}
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" placeholder="Type here" defaultValue={"Anvi"}/>
+      </>
+    );
+  }
 }
 
 export default App;
