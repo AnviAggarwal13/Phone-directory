414cb927c8f7aed1941c3b668496130feab47b17
AnviAggarwal13 <anviaggarwal13@gmail.com>
2022-12-19 00:23:21 +0530

    Styled header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from "react";
+import Header from './Header';
+import './App.css';
+
 class App extends Component {
   render() {
+    let sub=[
+      {
+        id:1,
+        name:"s",
+        phone:"999999"
+      },
+      {
+        id:2,
+        name:"h",
+        phone:"888888"
+      }
+    ]
     return (
       <>
-        {/* <div className="header">
-          Phone Directory
+       <Header />
+        <button className="addBtn">Add</button>
+        <div className="grid-container">
+          <span className="grid-item name">Name</span><br />
+          <span className="grid-item phone">Phone</span>
         </div>
-        <button>Add</button>
-        <div>
-          <span>Name</span><br />
-          <span>Phone</span>
+
+        {/* <div className="grid-container">
+          <span className="grid-item ">Srishti</span><br />
+          <span className="grid-item ">999999</span>
+        </div>
+
+        <div className="grid-container">
+          <span className="grid-item ">Shilpa</span><br />
+          <span className="grid-item ">888888</span>
         </div> */}
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" placeholder="Type here" defaultValue={"Anvi"}/>
+        {
+          sub.map(subs => {
+            return <div key={subs.id} className="grid-container">
+            <span className="grid-item">{subs.name}</span>
+            <span className="grid-item">{subs.phone}</span>
+            <span className="grid-item">
+              <button className="deleteBtn">Delete</button>
+            </span>
+            </div>
+          })
+        }
       </>
     );
   }
